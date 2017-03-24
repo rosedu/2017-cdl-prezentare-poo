baa7e7201c03baf1af396a575e284ab197ddb64d
Razvan Chitu <razvan.ch95@gmail.com>
2017-03-24 11:37:18 +0200

    1-messy: implement stack

--- 1-messy/stack.c
+++ 1-messy/stack.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 
+
 void push(int *v, int *n, int el)
 {
-	v[*n] = el;
-	(*n)++;
+    v[*n] = el;
+    (*n)++;
+}
+
+int top(int *v, int *n)
+{
+    return v[*n - 1];
 }
 
 int pop(int *v, int *n)
 {
-	int ret;
-	ret = v[*n - 1];
-  	
-  	(*n)--;
-  
-	return ret;
+    int ret;
+
+    ret = top(v, n);
+
+    (*n)--;
+
+    return ret;
 }
 
 int is_empty(int *v, int *n)
 {
-	return *n == 0;
+    return *n == 0;
 }
 
 int get_size(int *v, int *n)
 {
-	return *n;
+    return *n;
 }
 
 int main()
 {
-  int n = 0;
-  int v[10];
-  
-  push(v, &n, 1);
-  push(v, &n, 2);
-  push(v, &n, 3);
-  printf("%d\n", pop(v, &n));
-  printf("%d\n", pop(v, &n));
-  printf("%d\n", pop(v, &n));
-  
-  return 0;
+    int n = 0;
+    int v[10];
+
+    push(v, &n, 1);
+    push(v, &n, 2);
+    push(v, &n, 3);
+
+    printf("%d\n", pop(v, &n));
+    printf("%d\n", pop(v, &n));
+    printf("%d\n", pop(v, &n));
+
+    return 0;
 }
